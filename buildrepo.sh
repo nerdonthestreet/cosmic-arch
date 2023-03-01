ec079507900a1846eecc43be957c97c709dcd382
Jacob Kauffmann <jacob@system76.com>
2023-03-01 16:29:07 -0700

    Add PKGBUILD files and WIP build script

---
+++ buildrepo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in cosmic-applet-host cosmic-applets cosmic-applibrary cosmic-bg cosmic-comp cosmic-launcher cosmic-osd cosmic-panel cosmic-session cosmic-settings cosmic-settings-daemon xdg-desktop-portal-cosmic
+do
+    echo "Building $i..."
+    cd $i
+    makepkg -s
+    cd ..
+done
