f8df09984c38b08fc62d134d6735933dc33ea2e8
Jacob Kauffmann <jacob@system76.com>
2023-03-01 23:30:22 -0700

    fix: Remove obsolete cosmic-applet-host component

--- buildrepo.sh
+++ buildrepo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in cosmic-applet-host cosmic-applets cosmic-applibrary cosmic-bg cosmic-comp cosmic-launcher cosmic-osd cosmic-panel cosmic-session cosmic-settings cosmic-settings-daemon xdg-desktop-portal-cosmic
+for i in cosmic-applets cosmic-applibrary cosmic-bg cosmic-comp cosmic-launcher cosmic-osd cosmic-panel cosmic-session cosmic-settings cosmic-settings-daemon xdg-desktop-portal-cosmic
 do
     echo "Building $i..."
     cd PKGBUILDs/$i
