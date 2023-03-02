e9829d917c4bed2c7fe7f7507f92cd05d1e71c26
Jacob Kauffmann <jacob@system76.com>
2023-03-02 06:57:04 -0700

    feat: Reset directory when building

--- buildrepo.sh
+++ buildrepo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Reset the directory.
+echo "Warning: This script will reset the directory in five seconds."
+sleep 5
+git reset --hard
+git clean -ffd # Two f's to remove obsolete git repos.
+
 for i in cosmic-applets cosmic-applibrary cosmic-bg cosmic-comp cosmic-launcher cosmic-osd cosmic-panel cosmic-session cosmic-settings cosmic-settings-daemon xdg-desktop-portal-cosmic
 do
     echo "Building $i..."
