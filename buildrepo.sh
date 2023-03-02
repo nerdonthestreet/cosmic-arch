4a04c1232a1aab02b62ab08a066ef2936ea1c1c3
Jacob Kauffmann <jacob@system76.com>
2023-03-01 23:27:26 -0700

    fix: Change into and out of the correct directories

--- buildrepo.sh
+++ buildrepo.sh
@@ -3,7 +3,7 @@
 for i in cosmic-applet-host cosmic-applets cosmic-applibrary cosmic-bg cosmic-comp cosmic-launcher cosmic-osd cosmic-panel cosmic-session cosmic-settings cosmic-settings-daemon xdg-desktop-portal-cosmic
 do
     echo "Building $i..."
-    cd $i
+    cd PKGBUILDs/$i
     makepkg -s
-    cd ..
+    cd ../..
 done
